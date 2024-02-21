6daae8ab8d000052065d7489abc27cd6fd7c64e2
Kesang97 <kesangs99@gmail.com>
2024-02-21 23:14:11 +0530

    side bar content + settings page added

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -7,16 +7,21 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Admin Panel</title>
+    <title>Admin Panel- Dashboard</title>
     <?php require('includes/links.php') ?>
 </head>
 <body class="bg-light">
 
-    <div class="container-fluid bg-dark text-light p-3 d-flex align-items-center justify-content-between">
-        <h3 class="mb-0">ADMIN PANEL</h3>
-        <a href="logout.php" class="btn btn-light btn-sm">LOG OUT</a>
-    </div>
 
+<?php require('includes/header.php') ?>
+
+    <div class="container-fluid" id="main-content">
+        <div class="row">
+            <div class="col-lg-10 ms-auto p-4 overflow-hidden">
+                
+            </div>
+        </div>
+    </div>
 
 
 
